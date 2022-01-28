5d9c884127bec6cf8e1f78d6b33aea3b01ebd55f
IshwariHade <ishwari1613@gmail.com>
2022-01-28 10:53:20 +0530

    java test class

---
+++ src/test/java/javacode/TestClass.java
@@ -0,0 +1,22 @@
+package javacode;
+
+public class TestClass//PascalNamingConvention
+{
+	public static void main(String args[])
+	{
+//	public static void main//camel Case Naming Convention
+//	(String[] args) {
+//		int num1= 7;
+//		int num2= 8;
+//		int num3= 10;
+//		int result;
+//		result = num1+num2+num3;
+//		System.out.println("The addition of three numbers is: ");
+//		System.out.println(result);
+	System.out.println("HELLOOOO");
+	System.out.print("HIIII");
+	}
+}
+
+
+
